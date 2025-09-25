e3058ece60ecdc2c67114d9a57d43d7f573f6596
Qutub Ahmed <qutubahmedkhan@outlook.com>
2025-09-25 20:57:39 +0500

    troubleshoot in second section

--- src/components/Secondsection.jsx
+++ src/components/Secondsection.jsx
@@ -1,8 +1,9 @@
+import hook from "../assets/desktop/hook.jpg";
 
 function Secondsection() {
   return (
 
-    <section className="flex flex-col items-center justify-center h-auto p-8 md:p-12 bg-[url('../../Desktop_Version/JPEG_Backgrounds/hook.jpg')] bg-no-repeat bg-cover bg-center text-center">
+    <section className="flex flex-col items-center justify-center h-auto p-8 md:p-12 bg-no-repeat bg-cover bg-center text-center" style={{backgroundImage: `url(${hook})`}}>
         <p className='text-xl md:text-4xl text-white font-semibold pb-2 md:pb-10 px-8 md:px-0'>Most people paste the prompt... then get stuck.</p>
         <h1 className='text-2xl md:text-6xl text-white font-bold max-w-5xl mx-auto text-balance'>HERE'S HOW TO ACTUALLY TURN IT INTO <span className='text-emerald-300'>CASHFLOW IN 10 DAYS.</span></h1>
     </section>
