ce7f425257ff042c69f19ae7a5eae3a6bccf1cd1
Qutub Ahmed <qutubahmedkhan@outlook.com>
2025-09-26 20:28:08 +0500

    reduce the button size

--- src/components/Discountsection.jsx
+++ src/components/Discountsection.jsx
@@ -11,7 +11,8 @@ function Discountsection() {
         <p className="text-3xl font-semibold text-center text-balance">Guides like this usually sell for 97+...</p>
         <h1 className="text-4xl md:text-6xl max-w-2xl font-bold text-center text-balance leading-tight">But today, you can grab it for just <span className="text-emerald-600">$27!</span></h1>
 
-        <button className="text-2xl md:text-3xl px-5 md:px-15 py-5 rounded-lg text-white font-semibold bg-gradient-to-b from-emerald-500 via-emerald-600 to-emerald-900 hover:from-emerald-900 hover:to-emerald-500 shadow-lg">Yes, Give Me The 10-Day Blueprint!</button>
+        {/* <button className="text-2xl md:text-3xl px-5 md:px-15 py-5 rounded-lg text-white font-semibold bg-gradient-to-b from-emerald-500 via-emerald-600 to-emerald-900 hover:from-emerald-900 hover:to-emerald-500 shadow-lg">Yes, Give Me The 10-Day Blueprint!</button> */}
+        <button className="px-10 py-3 rounded-lg text-white font-semibold bg-gradient-to-b from-emerald-500 via-emerald-600 to-emerald-900 hover:from-emerald-900 hover:to-emerald-500 shadow-lg">Yes, Give Me The 10-Day Blueprint!</button>
     </section>
   )
 }
--- src/components/Guaranteesection.jsx
+++ src/components/Guaranteesection.jsx
@@ -16,7 +16,8 @@ function Guaranteesection() {
                 <p className='text-lg md:text-xl text-balance block md:hidden text-center'>If you don't feel like this saves you weeks of wasted trial and error, just let us know and we'll refund you</p>
                 <p className="text-2xl text-emerald-400 text-center md:text-left">DON'T JUST PASTE THE PROMPT AND HOPE FOR THE BEST.</p>
                 <p className='text-3xl font-semibold text-balance text-center md:text-left'>Follow the exact 10 steps that turned it into a faceless cash machine - starting today</p>
-                <button className="text-xl md:text-2xl px-10 py-5 rounded-lg text-white font-semibold bg-gradient-to-b from-emerald-500 via-emerald-600 to-emerald-900 hover:from-emerald-900 hover:to-emerald-500 shadow-lg">Yes, Give Me The 10-Day Blueprint!</button>
+                {/* <button className="text-xl md:text-2xl px-10 py-5 rounded-lg text-white font-semibold bg-gradient-to-b from-emerald-500 via-emerald-600 to-emerald-900 hover:from-emerald-900 hover:to-emerald-500 shadow-lg">Yes, Give Me The 10-Day Blueprint!</button> */}
+                <button className="px-10 py-3 rounded-lg text-white font-semibold bg-gradient-to-b from-emerald-500 via-emerald-600 to-emerald-900 hover:from-emerald-900 hover:to-emerald-500 shadow-lg">Yes, Give Me The 10-Day Blueprint!</button>
             </div>
             <div className="hidden-1200px absolute -z-10 top-0 left-auto right-0 h-full ">
 
